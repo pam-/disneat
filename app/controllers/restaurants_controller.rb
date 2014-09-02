6b673618871389dcc27b7f555b30cea27e4192df
pam- <pam.assogba@gmail.com>
2014-09-02 16:47:18 -0400

    done age specification for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
 	def index
 		@restaurants = Restaurant.all
+		@user = User.find(params[:user_id])
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,35 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
-	def method_name
-		
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else 
+			render 'new'
+		end 
+	end
+
+	def edit
+		@user.find(params[:id])
+	end
+
+	def update
+		@user.update(user_params)
+
+		if @user.update
+			redirect_to @user
+		else 
+			render 'edit'
+		end 
+	end
+
+	def destroy
+		@user = User.find(params[:id]).destroy
+		redirect_to '/'
 	end
 end
